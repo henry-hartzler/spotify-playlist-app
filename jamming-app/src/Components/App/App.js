8de199736fe60afb8a18207386139bec15f4c199
Henry Hartzler <hartzler.henry@gmail.com>
2022-06-11 15:09:16 -0600

    passed onAdd from Tracklist to Track

--- jamming-app/src/Components/App/App.js
+++ jamming-app/src/Components/App/App.js
@@ -50,6 +50,7 @@ class App extends React.Component {
       }
     ]
     };
+    this.addTrack = this.addTrack.bind(this);
   }
 
   addTrack(track) {
@@ -69,8 +70,10 @@ class App extends React.Component {
         <div className="App">
          <SearchBar />
           <div className="App-playlist">
-        <SearchResults searchResults={this.state.searchResults} />
-        <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
+        <SearchResults  searchResults={this.state.searchResults} 
+                        onAdd={this.addTrack} />
+        <Playlist   playlistName={this.state.playlistName} 
+                    playlistTracks={this.state.playlistTracks} />
           </div>
         </div>
       </div>
