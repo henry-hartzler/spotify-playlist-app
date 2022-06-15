2d27124830a0dc492d594a0cce05fcf1a78419b1
Henry Hartzler <hartzler.henry@gmail.com>
2022-06-15 11:45:07 -0600

    updated name of title to Beats Bayou

--- jamming-app/src/Components/App/App.js
+++ jamming-app/src/Components/App/App.js
@@ -64,7 +64,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <h1><span className="highlight">Be</span>ats Ba<span className="highlight">you</span></h1>
         <div className="App">
          <SearchBar onSearch={this.search} />
           <div className="App-playlist">
